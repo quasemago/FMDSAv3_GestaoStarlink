120c520d6728c71d9d9dcd5e19a9337d4c2576de
quasemago <brunoronningfn@gmail.com>
2024-06-14 19:05:53 -0400

    feat(backend): Específica os atributos que serão retornados na consulta dos clientes.

--- backend/app/controller/ClientController.js
+++ backend/app/controller/ClientController.js
@@ -1,19 +1,20 @@
 import Client from "../database/models/Client.js";
+import Account from "../database/models/Account.js";
 
 class ClientController {
     async getAll(req, res) {
-        if (req.userRole !== 'ADMIN') {
-            res.status(403)
-                .json({message: "Acesso negado!"});
-            return;
-        }
-
         try {
             const clients = await Client.findAll({
-                order: [['id', 'DESC']]
+                attributes: ['id', 'name', 'tel', 'address', 'birthDate', 'gender', 'profilePicture'],
+                order: [['id', 'DESC']],
+                include: {
+                    model: Account,
+                    attributes: ['email', 'role']
+                }
             });
             return res.json(clients);
         } catch (err) {
+            app_logger.error(err.message);
             return res.status(400).json({
                 message: ERROR_MESSAGES.GENERIC_ERROR
             });
@@ -21,12 +22,6 @@ class ClientController {
     }
 
     async getById(req, res) {
-        if (req.userRole !== 'ADMIN') {
-            res.status(403)
-                .json({message: "Acesso negado!"});
-            return;
-        }
-
         try {
             const {id} = req.params;
             if (!id) {
@@ -35,7 +30,13 @@ class ClientController {
                 });
             }
 
-            const client = await Client.findByPk(id);
+            const client = await Client.findByPk(id, {
+                attributes: ['id', 'name', 'tel', 'address', 'birthDate', 'gender', 'profilePicture'],
+                include: {
+                    model: Account,
+                    attributes: ['email', 'role']
+                }
+            });
             if (!client) {
                 return res.status(404).json({
                     message: 'Cliente com o id informado não encontrado.'
@@ -44,6 +45,7 @@ class ClientController {
 
             return res.json(client);
         } catch (err) {
+            app_logger.error(err);
             return res.status(400).json({
                 message: ERROR_MESSAGES.GENERIC_ERROR
             });
